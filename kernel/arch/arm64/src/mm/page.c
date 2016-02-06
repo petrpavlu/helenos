82fd398c2bdf76cc10ed6f3190856996c3041fc9
Petr Pavlu <setup@dagobah.cz>
2016-02-06 11:48:38 +0000

    Implement page_arch_init() for ARM64

--- kernel/arch/arm64/src/mm/page.c
+++ kernel/arch/arm64/src/mm/page.c
@@ -41,8 +41,25 @@
 /** Initializes page tables. */
 void page_arch_init(void)
 {
-	/* REVISIT */
+	if (config.cpu_active > 1) {
+		as_switch(NULL, AS_KERNEL);
+		return;
+	}
+
 	page_mapping_operations = &pt_mapping_operations;
+
+	page_table_lock(AS_KERNEL, true);
+
+	/* PA2KA(identity) mapping for all low-memory frames. */
+	for (uintptr_t cur = 0; cur < config.identity_size; cur += FRAME_SIZE) {
+		uintptr_t addr = physmem_base + cur;
+		page_mapping_insert(AS_KERNEL, PA2KA(addr), addr,
+		    PAGE_GLOBAL | PAGE_CACHEABLE | PAGE_EXEC | PAGE_WRITE |
+		    PAGE_READ);
+	}
+
+	page_table_unlock(AS_KERNEL, true);
+
 	as_switch(NULL, AS_KERNEL);
 }
 
