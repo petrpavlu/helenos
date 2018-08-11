9dd0b132d39e28d89c35756de9edc8cff426d1d9
Petr Pavlu <setup@dagobah.cz>
2018-08-11 19:10:40 +0100

    arm64: Implement basic CPU identification

--- kernel/arch/arm64/include/arch/cpu.h
+++ kernel/arch/arm64/include/arch/cpu.h
@@ -36,9 +36,19 @@
 #ifndef KERN_arm64_CPU_H_
 #define KERN_arm64_CPU_H_
 
-/* REVISIT */
 /** Struct representing ARM CPU identification. */
 typedef struct {
+	/** Implementer (vendor) number. */
+	uint32_t implementer;
+
+	/** Variant number. */
+	uint32_t variant;
+
+	/** Primary part number. */
+	uint32_t partnum;
+
+	/** Revision number. */
+	uint32_t revision;
 } cpu_arch_t;
 
 #endif
--- kernel/arch/arm64/src/cpu/cpu.c
+++ kernel/arch/arm64/src/cpu/cpu.c
@@ -33,24 +33,64 @@
  * @brief CPU identification.
  */
 
+#include <arch/regutils.h>
 #include <cpu.h>
+#include <typedefs.h>
+
+/** Decode implementer (vendor) name. */
+static const char *implementer(uint32_t id)
+{
+	switch (id) {
+	case 0x41:
+		return "ARM Limited";
+	case 0x42:
+		return "Broadcom Corporation";
+	case 0x43:
+		return "Cavium Inc.";
+	case 0x44:
+		return "Digital Equipment Corporation";
+	case 0x49:
+		return "Infineon Technologies AG";
+	case 0x4d:
+		return "Motorola or Freescale Semiconductor Inc.";
+	case 0x4e:
+		return "NVIDIA Corporation";
+	case 0x50:
+		return "Applied Micro Circuits Corporation";
+	case 0x51:
+		return "Qualcomm Inc.";
+	case 0x56:
+		return "Marvell International Ltd.";
+	case 0x69:
+		return "Intel Corporation";
+	}
+	return "Unknown implementer";
+}
 
 /** Perform ARM64-specific tasks needed for CPU initialization. */
 void cpu_arch_init(void)
 {
-	/* REVISIT */
 }
 
 /** Retrieve processor identification and stores it to #CPU.arch */
 void cpu_identify(void)
 {
-	/* REVISIT */
+	uint64_t midr = MIDR_EL1_read();
+
+	CPU->arch.implementer =
+	    (midr & MIDR_IMPLEMENTER_MASK) >> MIDR_IMPLEMENTER_SHIFT;
+	CPU->arch.variant = (midr & MIDR_VARIANT_MASK) >> MIDR_VARIANT_SHIFT;
+	CPU->arch.partnum = (midr & MIDR_PARTNUM_MASK) >> MIDR_PARTNUM_SHIFT;
+	CPU->arch.revision = (midr & MIDR_REVISION_MASK) >> MIDR_REVISION_SHIFT;
 }
 
 /** Print CPU identification. */
 void cpu_print_report(cpu_t *m)
 {
-	/* REVISIT */
+	printf("cpu%d: vendor=%s, variant=%" PRIx32 ", part number=%" PRIx32
+	       ", revision=%" PRIx32 "\n",
+	    m->id, implementer(m->arch.implementer), m->arch.variant,
+	    m->arch.partnum, m->arch.revision);
 }
 
 /** @}
