439ba38bf77d3554f3b4967a99ed06fc47e91e2f
Petr Pavlu <setup@dagobah.cz>
2019-03-09 17:11:26 +0000

    Remove no longer needed downstream changes

--- boot/generic/include/payload.h
+++ boot/generic/include/payload.h
@@ -29,15 +29,15 @@
 #ifndef BOOT_PAYLOAD_H_
 #define BOOT_PAYLOAD_H_
 
-#include <arch/asm.h>
 #include <arch/types.h>
 #include <stddef.h>
 #include <stdint.h>
 
-extern uint8_t loader_start[];
-extern uint8_t loader_end[];
 extern uint8_t payload_start[];
 extern uint8_t payload_end[];
+
+extern uint8_t loader_start[];
+extern uint8_t loader_end[];
 
 size_t payload_unpacked_size(void);
 void extract_payload(taskmap_t *, uint8_t *, uint8_t *, uintptr_t,
