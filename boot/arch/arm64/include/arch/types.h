834cbcbd470ffc41761f678484211012bf9ba234
Petr Pavlu <setup@dagobah.cz>
2019-03-09 16:53:10 +0000

    arm64: Share again boot types between the kernel and bootloader

--- boot/arch/arm64/include/arch/types.h
+++ boot/arch/arm64/include/arch/types.h
@@ -38,63 +38,8 @@
 
 #include <_bits/all.h>
 
-#define BOOTINFO_TASK_NAME_BUFLEN  32
-#define TASKMAP_MAX_RECORDS        32
-#define MEMMAP_MAX_RECORDS        128
-
-/** Task structure. */
-typedef struct {
-	/** Address where the task was placed. */
-	void *addr;
-	/** Size of the task's binary. */
-	size_t size;
-	/** Task name. */
-	char name[BOOTINFO_TASK_NAME_BUFLEN];
-} task_t;
-
-/** Task map structure. */
-typedef struct {
-	/** Number of boot tasks. */
-	size_t cnt;
-	/** Boot task data. */
-	task_t tasks[TASKMAP_MAX_RECORDS];
-} taskmap_t;
-
-/** Memory zone types. */
-typedef enum {
-	/** Unusuable memory. */
-	MEMTYPE_UNUSABLE,
-	/** Usable memory. */
-	MEMTYPE_AVAILABLE,
-	/** Memory that can be used after ACPI is enabled. */
-	MEMTYPE_ACPI_RECLAIM
-} memtype_t;
-
-/** Memory area. */
-typedef struct {
-	/** Type of the memory. */
-	memtype_t type;
-	/** Address of the area. */
-	void *start;
-	/** Size of the area. */
-	size_t size;
-} memzone_t;
-
-/** System memory map. */
-typedef struct {
-	/** Number of memory zones. */
-	size_t cnt;
-	/** Memory zones. */
-	memzone_t zones[MEMMAP_MAX_RECORDS];
-} memmap_t;
-
-/** Bootinfo structure. */
-typedef struct {
-	/** Task map. */
-	taskmap_t taskmap;
-	/** Memory map. */
-	memmap_t memmap;
-} bootinfo_t;
+#include <arch/boot.h>
+#define task_t utask_t
 
 #endif
 
